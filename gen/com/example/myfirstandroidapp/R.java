153a027f60968b5ac59c8835821136831fdcc583
Matheus Moreira <mvrmoreira@gmail.com>
2014-02-24 09:10:11 -0300

    criando a search activity

--- gen/com/example/myfirstandroidapp/R.java
+++ gen/com/example/myfirstandroidapp/R.java
@@ -32,11 +32,13 @@ public final class R {
     public static final class layout {
         public static final int activity_display_message=0x7f030000;
         public static final int activity_main=0x7f030001;
+        public static final int activity_search=0x7f030002;
     }
     public static final class menu {
         public static final int display_message=0x7f070000;
         public static final int main=0x7f070001;
         public static final int main_activity_actions=0x7f070002;
+        public static final int search=0x7f070003;
     }
     public static final class string {
         public static final int action_search=0x7f050006;
@@ -46,6 +48,7 @@ public final class R {
         public static final int edit_message=0x7f050001;
         public static final int hello_world=0x7f050004;
         public static final int title_activity_display_message=0x7f050005;
+        public static final int title_activity_search=0x7f050007;
     }
     public static final class style {
         /** 
--- src/com/example/myfirstandroidapp/MainActivity.java
+++ src/com/example/myfirstandroidapp/MainActivity.java
@@ -5,6 +5,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.EditText;
 
@@ -26,7 +27,32 @@ public class MainActivity extends Activity {
         return super.onCreateOptionsMenu(menu);
     }
     
-    public void sendMessage(View view) {
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle presses on the action bar items
+        switch (item.getItemId()) {
+            case R.id.action_search:
+                openSearch();
+                return true;
+            case R.id.action_settings:
+                openSettings();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+    
+    private void openSettings() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	private void openSearch() {
+		Intent intent = new Intent(this, SearchActivity.class);
+    	startActivity(intent);		
+	}
+
+	public void sendMessage(View view) {
     	Intent intent = new Intent(this, DisplayMessageActivity.class);
     	EditText editText = (EditText) findViewById(R.id.edit_message);
     	String message = editText.getText().toString();
